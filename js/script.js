7351a64aee5cb24b1d8e713066c38cd08c655cf5
JULIA GALVAO ALMEIDA SANTOS <julia.gasantos@senacsp.edu.br>
2025-02-28 22:17:04 -0300

    formulario de contato

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// video
+window.addEventListener("load", () => {
+  let video = window.document.querySelector("#video");
+  video.play();
+  video.loop = true;
+});
+
+//temas
+const btnClaro = document.getElementById("lightMode");
+const btnEscuro = document.getElementById("darkMode");
+const body = document.body;
+body.classList.add("lightMode");
+btnClaro.style.display = "inline";
+btnEscuro.style.display = "none";
+
+btnClaro.addEventListener("click", () => {
+  body.classList.remove("lightMode");
+  body.classList.add("darkMode");
+  btnClaro.style.display = "none";
+  btnEscuro.style.display = "inline";
+});
+
+btnEscuro.addEventListener("click", () => {
+  body.classList.remove("darkMode");
+  body.classList.add("lightMode");
+  btnEscuro.style.display = "none";
+  btnClaro.style.display = "inline";
+});
+
+//active
+document.querySelectorAll('.navLink li').forEach((item) => {
+  item.addEventListener('click', function () {
+    // remove a classe active de todos os itens
+    document.querySelectorAll('navLink li').forEach((li) => {
+      li.classList.remove('active');
+    });
+
+    //adiciona a classe active ao item classe
+    this.classList.add('active');
+  });
+});
+
+//escondendo o span
+document.addEventListener("DOMContentLoaded", function () {
+  const navBar = document.querySelector(".navBar");
+  const spans = document.querySelectorAll(".navLink li a span");
+
+  function toggleSpanVisibility() {
+    if (navBar.offsetWidth > 4 * 16) { //converte 4rem em pixels
+      spans.forEach((span) => {
+        span.style.opacity = "0";
+        span.style.opacity = "visible";
+      });
+    }
+    else {
+      spans.forEach((span) => {
+        span.style.opacity = "1";
+        span.style.opacity = "hidden";
+      });
+    }
+  }
+//esconde os spans inicialmente
+  toggleSpanVisibility();
+
+  navBar.addEventListener("mouseenter", () => {
+    spans.forEach((span) => {
+      span.style.opacity = "1";
+      span.style.opacity = "visible";
+    });
+  });
+
+  navBar.addEventListener("mouseleave", () => {
+    spans.forEach((span) => {
+      span.style.opacity = "0";
+      span.style.opacity = "hidden";
+    });
+  });
+});
